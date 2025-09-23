4ae1179f32e662a890964cccd216642c8bf7dd02
timcodes <timcodes117@gmail.com>
2025-09-23 16:35:10 +0100

    ai chatbot endpoint and small frontend improvements

--- flask_backend/app/routes/ai_chat.py
+++ flask_backend/app/routes/ai_chat.py
@@ -12,3 +12,18 @@ ai_chatbot_bp = Blueprint('users', __name__)
 def list_users():
     return jsonify({'success': True, 'data': "respones"}), 200
 
+
+
+# hey victor
+# this is just a comment to help you create endpoints
+# if you want to create a new route, start by creating a new file in the same folder as this one
+
+# then, create a bluePrint - this is just like the api router in FAST API
+# blueprint_name = Blueprint('route_name', __name__) 
+
+# to create an endpoint 
+# use the blueprint decorator to call an api method [GET, POST, PUT, ...OTHERS]
+# if you want to make the endpoint secure by expecting an access Token from the from the frontend
+# continue with the @jwt_required() decorator before calling the api function
+#  then create the api function
+# then do your logic and return a valid response conditionally or not.
